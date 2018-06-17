a6abdae900069a8c40ebb7dfd42c10d5430b23ad
Glitch (fcc-bar) <none>
2018-06-17 10:44:36 +0000

    Fix tips on x-axis plus test on  tooltips

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
 
-// prints "hi" in the browser's dev tools console
-const w = 1000
-const h = w * 3 / 4
+const w = 1100
+const h = w * 9 / 16
 const wPadding = w *0.1 
 const hPadding = h *0.2 
 const svg = d3.select("#fcc-bar")
@@ -18,7 +17,7 @@ svg.append('text')
   .text("That's Gross")
 svg.append('text')
   .attr('x', w / 2)
-  .attr('y', h  -hPadding + 20)
+  .attr('y', h  - hPadding + 40)
   .text("That's your Date")
 const tooltip = d3.select("#fcc-bar")
   .append("div")
@@ -27,19 +26,16 @@ const tooltip = d3.select("#fcc-bar")
 // Get the Data
 d3.json("data.json",
     (data) => {
+      console.log(data.data)
       const rectWidth = (w - 2 * wPadding) / data.data.length
       const yMax = d3.max(data.data, d => d[1])
       const yMin = d3.min(data.data, d => d[1])
       const diff = yMax - yMin
       
-      const years = data.data.map(d => d[0].slice(0,4))
+      const years = data.data.map(d => parseInt(d[0].slice(0,4)))
       
       const xMin = d3.min(years)
-      const xMax = d3.min(years)
-      
-      console.log(years)
-
-
+      const xMax = d3.max(years)
       
       const yScale = d3.scaleLinear()
         .domain([0, yMax+ diff / 10])
@@ -63,16 +59,22 @@ d3.json("data.json",
         .attr("fill", "white")
         .on("mouseover", (d,i) => {
             tooltip.attr("data-date", d[0])
-              .style("left", `${wPadding + rectWidth * i +20}`)
-              .style("top", `${h/2}`)
-              .html(`<br><strong>${d[0]}</strong>`)
+              .style("left", `${wPadding + rectWidth * i + 260}px`)
+              .style("top", `${h*2/3}px`)
+              .style("display", "inline-block")
+              .html(`<br><strong>${d[0]}</strong><br>${d[1]}`)
             })
-
+        .on("mouseleave",
+            (d,i) => {
+              tooltip.style("display", "none")
+            }
+           )
+  
       const yAxis = d3.axisLeft().scale(yScale)
         
       const xAxis = d3.axisBottom()
         .scale(xScale)
-        
+        .tickFormat(d3.format("d"));
   
       svg.append("g")
         .attr("id", "y-axis")
@@ -82,9 +84,6 @@ d3.json("data.json",
         .attr("id", "x-axis")
         .attr("transform", `translate(0, ${h - hPadding})`)
         .call(xAxis)
-
-
-  
   }
 )
 /*
